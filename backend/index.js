86f2651437714134da647e51e16417236413fc1c
smallik1995 <smallik666@outlook.com>
2024-04-05 13:25:27 +0300

    init commit

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const router = require('./routes/router');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const corsOption = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200
+}
+
+app.use(cors(corsOption));
+app.use('/', router);
+
+const port = 3001;
+const server = app.listen(port, () => {
+    console.log('server start')
+})
---
+++ src/app/pages/test/page.tsx
@@ -0,0 +1,5 @@
+const Test = () => {
+    return <div>123</div>
+}
+
+export default Test;
